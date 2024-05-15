2183b7f087f6df03be793855bd94c9ad57a8612e
abritor <araybrito2005@gmail.com>
2024-05-15 21:14:06 +0200

    fetch a mitad para acceder a peliculas

--- jvscript/main.js
+++ jvscript/main.js
@@ -1,58 +1,43 @@
-function fetchPokemon(nombre) {
+function getPeli(nombre) {
     console.log(nombre);
-    fetch(`https://localhost:8080/api/v1/pelicula/${nombre}/`)
+    fetch(`http://172.30.134.216:8080/api/v1/pelicula/${nombre}`)
       .then((res) => res.json())
       .then((data) => {
-        //createPokemon(data);
-        //spinner.style.display = "none";
-        console.log(data);
-    });
+        crearPelicula(data);
+        console.log();
+      });
+  }
+
+function getPelicula() {
+    let nombre = document.getElementById("nombrePeli").value;
+    getPeli(nombre);
 }
+function crearPelicula(){
+    console.log(pelicula);
 
-function fetchPokemons() {
-    let nombre = document.getElementById("busqueda").value;
-    spinner.style.display = "block";
-    fetchPokemon(nombre);
-}
-
-
-function createPokemon(pokemon) {
-    console.log(pokemon);
-    const flipCard = document.createElement("div");
-    flipCard.classList.add("flip-card");
-  
-    const cardContainer = document.createElement("div");
-    cardContainer.classList.add("card-container");
-  
-    flipCard.appendChild(cardContainer);
-  
-    const card = document.createElement("div");
-    card.classList.add("pokemon-block");
-  
     const spriteContainer = document.createElement("div");
-    spriteContainer.classList.add("img-container");
-  
-    const sprite = document.createElement("img");
-    sprite.src = pokemon.sprites.front_default;
-  
+    spriteContainer.classList.add("pelicula_div");
+
+    const nombre = document.createElement("p");
+    nombre.textContent = pelicula.titulo;
     spriteContainer.appendChild(sprite);
-  
+
     const number = document.createElement("p");
     number.textContent = `#${pokemon.id.toString().padStart(3, 0)}`;
-  
+
     const name = document.createElement("p");
     name.classList.add("name");
     name.textContent = pokemon.name;
-  
+
     card.appendChild(spriteContainer);
     card.appendChild(number);
     card.appendChild(name);
-  
+
     const cardBack = document.createElement("div");
     cardBack.classList.add("pokemon-block-back");
-  
+
     cardBack.appendChild(progressBars(pokemon.stats));
-  
+
     cardContainer.appendChild(card);
     cardContainer.appendChild(cardBack);
     pokemonContainer.appendChild(flipCard);
