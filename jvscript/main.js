866e52f0fa981a1a5f6de505993fb3545d370135
araybr <araybrito2005@gmail.com>
2024-05-16 00:20:10 +0200

    Prueba de subida de javascpit

--- jvscript/main.js
+++ jvscript/main.js
@@ -1,12 +1,23 @@
 function getPeli(nombre) {
     console.log(nombre);
-    fetch(`http://localhost:8080/api/v1/pelicula/${nombre}`)
-      .then((res) => res.json())
-      .then((data) => {
-        crearPelicula(data);
-        console.log();
-      });
-  }
+    fetch(`http://localhost:8080/api/v1/pelicula/${nombre}`, {
+    method: 'GET',
+    headers: {
+      'accept': 'application/json',
+      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZjZiNjg4MDlkZGMyYTAyOGZmNzZiODY3ZWE5ZjI5MCIsInN1YiI6IjY2NDRjYzAwOGU2NDk3ZWY2ZTViY2JjZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MstHGYLhi2JqMKg98iEOknnVws5W12bYu5jRRSu7WN4'
+    }
+  })
+  .then((res) => {
+    if (!res.ok) {
+      throw new Error('Network response was not ok ' + res.statusText);
+    }
+    return res.json();
+  })
+  .then((data) => {
+    crearPelicula(data);
+  })
+  .catch((error) => console.error('Error:', error));
+}
 
 function getPelicula() {
     let nombre = document.getElementById("nombrePeli").value;
