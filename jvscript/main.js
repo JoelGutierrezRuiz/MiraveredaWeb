3db36b73b4a09b651e9fc467ed2bb30eb06bf03b
araybr <araybrito2005@gmail.com>
2024-05-16 00:38:50 +0200

    Intento de arreglar el desasatre

--- jvscript/main.js
+++ jvscript/main.js
@@ -1,32 +1,21 @@
 function getPeli(nombre) {
     console.log(nombre);
     fetch(`http://localhost:8080/api/v1/pelicula/${nombre}`, {
-    method: 'GET',
-    headers: {
-      'accept': 'application/json',
-      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZjZiNjg4MDlkZGMyYTAyOGZmNzZiODY3ZWE5ZjI5MCIsInN1YiI6IjY2NDRjYzAwOGU2NDk3ZWY2ZTViY2JjZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MstHGYLhi2JqMKg98iEOknnVws5W12bYu5jRRSu7WN4'
-    }
   })
-  .then((res) => {
-    if (!res.ok) {
-      throw new Error('Network response was not ok ' + res.statusText);
-    }
-    return res.json();
-  })
-  .then((data) => {
-    crearPelicula(data);
-  })
-  .catch((error) => console.error('Error:', error));
+  .then((res) => res.json())
+    .then((data) => {
+      crearPelicula(data);
+    });
 }
 
 function getPelicula() {
     let nombre = document.getElementById("nombrePeli").value;
     getPeli(nombre);
 }
-function crearPelicula(){
+function crearPelicula(pelicula){
     console.log(pelicula);
 
-    const conte = getElementById("componentes");
+    const conte = document.getElementById("componentes");
 
     const spriteContainer = document.createElement("div");
     spriteContainer.classList.add("pelicula_div");
